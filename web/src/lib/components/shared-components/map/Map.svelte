781e907d7cfb90c250a6e6466c8a3708e91332e9
Miguel Raposo <miguel.p.raposo@tecnico.ulisboa.pt>
2026-06-08 12:49:45 +0100

    fix: add map mock network for e2e tests
    Co-authored-by: Afonso Mendonça Ribeiro <afonso.mendonca.ribeiro@tecnico.ulisboa.pt>

--- web/src/lib/components/shared-components/map/Map.svelte
+++ web/src/lib/components/shared-components/map/Map.svelte
@@ -399,7 +399,7 @@
     {#if onToggleTimeline}
       <Control position="top-right">
         <ControlGroup>
-          <ControlButton onclick={() => onToggleTimeline?.()}>
+          <ControlButton title={$t('timeline')} onclick={() => onToggleTimeline?.()}>
             <Icon
               title={$t('timeline')}
               icon={mdiImageMultiple}
